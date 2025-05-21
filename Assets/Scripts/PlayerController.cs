82a2e4f9ba06846f3819254be0cbc177b8016a78
jeonghyerin002 <jhr102446@g.shingu.ac.kr>
2025-05-21 12:03:22 +0900

    아 까먹었다

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float moveSpeed = 1f;
+
+    [SerializeField] Sprite spriteUp;
+    [SerializeField] Sprite spriteDown;
+    [SerializeField] Sprite spriteLeft;
+    [SerializeField] Sprite spriteRight;
+
+    Rigidbody2D rb;
+    SpriteRenderer sR;
+
+    Vector2 input;
+    Vector2 Velocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sR = GetComponent<SpriteRenderer>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
